b488da2148c530a8ff90faae21c62a611bd67d8a
ianalexeimt <ianalexeimt@gmail.com>
2026-05-16 10:54:17 -0600

    Crea la funcion que procesa lineas completas

--- semana-01/Main.py
+++ semana-01/Main.py
@@ -9,3 +9,17 @@ def value_cleaner (dirty_value):
         if character in accepted:
             filtered+=character
     return filtered        
+
+def line_processor (original_line):
+    total=0
+    parts=original_line.split(',')
+
+    for part in parts:
+        clean_part=value_cleaner(part)
+        try:
+            clean_part=float(clean_part)
+            clean_part=int(clean_part)
+            total+=clean_part
+        except ValueError:
+            pass
+    return total
