541d0121ba4d1007f9646a46e9518ffefb27536a
ianalexeimt <ianalexeimt@gmail.com>
2026-05-18 18:59:26 -0600

    Empieza main

--- semana-03/Main.py
+++ semana-03/Main.py
@@ -18,3 +18,24 @@ def line_parser (original_line):
     except ValueError:
         pass
 
+def main():
+    productos={}
+    is_first_line=True
+
+    for line in sys.stdin:
+        clean_line=line.strip()
+        if is_first_line:
+            is_first_line=False
+            continue
+
+        if not clean_line:
+            continue
+
+        parsed_result=line_parser(clean_line)
+        if parsed_result:
+            producto=parsed_result[0]
+            cantidad=parsed_result[1]
+            precio=parsed_result[2]
+
+            if producto not in productos:
+                productos[producto]={'unidades': 0, 'ingreso': 0.0}
